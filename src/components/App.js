2c713eafd00cf9c3f87727a0674213c447023a50
An Nguyen <nguyenan8196@gmail.com>
2021-10-01 18:03:39 +0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import User from "./User";
+
+class App extends Component {
+    render() {
+        return <div className='container m-auto'>
+            <User/>
+        </div>
+    }
+}
+
+export default App;
