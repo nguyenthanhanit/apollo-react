74070c26534f6e925d6e03ee4c798139f51f79dc
An Nguyen <nguyenan8196@gmail.com>
2021-10-08 11:06:07 +0700

    tree

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -23,51 +23,37 @@ const GET_DATA = gql`
 export default function Tree(props) {
     const authors = _.get(useQuery(GET_DATA), 'data.getAuthors', []);
 
-    const renderBody = (data, key, parent) => {
-        let parentRoute = '';
-        if (parent[key].__typename === 'Chapter') {
-            parentRoute = 'Comic/'
-        }
-
-        return (
-            <div>
-                {
-                    _.map(data, function (value, name) {
-                        if (name === 'id' || name === '__typename') return;
-                        if (_.isArray(value)) {
-                            return (
-                                <div className='ml-20 relative'>
-                                    {renderTable(value)}
-                                </div>
-                            )
-                        }
-                        return (
-                            <div className='relative'>
-                                <div className='absolute h-1 w-5 bg-blue-500 top-1/2'/>
-                                <div className='absolute h-5 w-1 bg-blue-500' style={{top: -4}}/>
-                                <span className='ml-8'>
-                                    <Link to={`${parentRoute + parent[key].__typename}/${parent[key].id}`}
-                                          className='underline'>{value}</Link>
-                                </span>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
+    const renderRow = (data, level = 0) => {
+        return _.map(data, function (value, name) {
+            if (name === 'id' || name === '__typename') return;
+            if (_.isArray(value)) {
+                return renderRow(value, level + 1)
+            }
+            if (_.isObject(value)) {
+                return renderRow(value, level)
+            }
+            return (
+                <tr>
+                    <td className={!level ? 'border-blue-500 border-b' : ''}>{!level ? data.name : ''} {level}</td>
+                    <td className={level === 1 ? 'border-blue-500 border-b border-l' : ''}>{level === 1 ? data.name : ''} {level}</td>
+                    <td className={level === 2 ? 'border-blue-500 border-b border-l' : ''}>{level === 2 ? data.name : ''} {level}</td>
+                </tr>
+            )
+        })
     }
 
-    const renderTable = (data, key) => {
-        return (
-            <div className={key ? 'ml-8' : ''}>
-                {
-                    _.map(data, function (value, key) {
-                        return renderBody(value, key, data)
-                    })
-                }
-            </div>
-        )
-    }
-
-    return renderTable(authors)
+    return (
+        <table className='w-full'>
+            <tr>
+                <th>Author</th>
+                <th>Comic</th>
+                <th>Chapter</th>
+            </tr>
+            {
+                _.map(authors, function (author) {
+                    return renderRow(author, 0)
+                })
+            }
+        </table>
+    )
 }
