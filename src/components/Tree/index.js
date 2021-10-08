552f18a29595c1c01a171d74284c21d69ee06b94
An Nguyen <nguyenan8196@gmail.com>
2021-10-08 12:24:33 +0700

    fix tree

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -20,23 +20,48 @@ const GET_DATA = gql`
     }
 `;
 
-export default function Tree(props) {
+export default function Tree() {
     const authors = _.get(useQuery(GET_DATA), 'data.getAuthors', []);
+    const columns = ['Authors', 'Comics', 'Chapters'];
 
-    const renderRow = (data, level = 0) => {
+    const renderHeader = (column) => {
+        return (
+            <th>{column}</th>
+        )
+    }
+
+    const renderRow = (data, path, level = 0) => {
         return _.map(data, function (value, name) {
             if (name === 'id' || name === '__typename') return;
+            const newPath = [...path, name];
             if (_.isArray(value)) {
-                return renderRow(value, level + 1)
+                return renderRow(value, newPath, level + 1)
             }
             if (_.isObject(value)) {
-                return renderRow(value, level)
+                return renderRow(value, newPath, level)
             }
+            const typename = _.get(authors, [...path, '__typename']);
+            const id = _.get(authors, [...path, 'id']);
             return (
                 <tr>
-                    <td className={!level ? 'border-blue-500 border-b' : ''}>{!level ? data.name : ''} {level}</td>
-                    <td className={level === 1 ? 'border-blue-500 border-b border-l' : ''}>{level === 1 ? data.name : ''} {level}</td>
-                    <td className={level === 2 ? 'border-blue-500 border-b border-l' : ''}>{level === 2 ? data.name : ''} {level}</td>
+                    <td className={!level ? 'border-blue-500 border-b' : ''}>
+                        {
+                            !level &&
+                            <Link to={`${typename}/${id}`}>{data.name}</Link>
+                        }
+                    </td>
+                    <td className={level === 1 ? 'border-blue-500 border-b border-l' : ''}>
+                        {
+                            level === 1 &&
+                            <Link to={`${typename}/${id}`}>{data.name}</Link>
+                        }
+                    </td>
+                    <td className={level === 2 ? 'border-blue-500 border-b border-l' : ''}>
+                        {
+                            level === 2 &&
+                            <Link to={`${typename}/${id}`}>{data.name}</Link>
+                        }
+                    </td>
                 </tr>
             )
         })
@@ -45,13 +70,13 @@ export default function Tree(props) {
     return (
         <table className='w-full'>
             <tr>
-                <th>Author</th>
-                <th>Comic</th>
-                <th>Chapter</th>
+                {
+                    _.map(columns, renderHeader)
+                }
             </tr>
             {
-                _.map(authors, function (author) {
-                    return renderRow(author, 0)
+                _.map(authors, function (author, index) {
+                    return renderRow(author, [index])
                 })
             }
         </table>
